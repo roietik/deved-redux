406da0a2fd92c3463c0d6dd2dbfed36814aa2c25
Radosław Grzymała <radoslaw.grzymala@sensilabs.pl>
2020-02-15 13:35:14 +0100

    async dispatch

--- src/service/api/FetchApi.js
+++ src/service/api/FetchApi.js
@@ -1,66 +1,68 @@
-
-const BASE_URL = "http://localhost:3015/counts"
+const BASE_URL = "http://localhost:4040/counts";
 
 const FetchApi = {
-    getFetch: async function () {
-        const res = await fetch(BASE_URL, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json"
-            }        
-        });
-        if (!res.ok) {
-            throw new Error("Something went wrong");
-        }
-        const items = await res.json();
-        return items;
-    },
-    addFetch: async function (itemToAdd) {
-        console.log('addFetch:' , itemToAdd);
-        const res = await fetch(BASE_URL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(itemToAdd)
-        });
-        if (!res.ok) {
-            throw new Error("Something went wrong");
-        }
-        const addedItem = await res.json();       
-        return addedItem;
-    },
-    replaceFetch: async function (itemToReplace) {
-        if (!itemToReplace.id) {
-            throw new Error("Item has to have an id to be updated")
-        }
-        const res = await fetch(`${BASE_URL}/${itemToReplace.id}`, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json"            },
-            body: JSON.stringify(itemToReplace)
-        });
-        if (!res.ok) {
-            throw new Error("Something went wrong");
-        }
-        const replacedItem = await res.json();       
-        return replacedItem;
-    },
-    removeFetch: async function (itemRemove) {
-        if (!itemRemove.id) {
-            throw new Error("Item has to have an id to be updated")
-        }
-        const res = await fetch(`${BASE_URL}/${itemRemove.id}`, {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json"            }        
-        });
-        if (!res.ok) {
-            throw new Error("Something went wrong");
-        }
-        const removedItem = await res.json();       
-        return removedItem;
+  getFetch: async function() {
+    const res = await fetch(BASE_URL, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    if (!res.ok) {
+      throw new Error("Something went wrong");
     }
-}
+    const items = await res.json();
+    return items;
+  },
+  addFetch: async function(itemToAdd) {
+    console.log("addFetch:", itemToAdd);
+    const res = await fetch(BASE_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(itemToAdd)
+    });
+    if (!res.ok) {
+      throw new Error("Something went wrong");
+    }
+    const addedItem = await res.json();
+    return addedItem;
+  },
+  replaceFetch: async function(itemToReplace) {
+    if (!itemToReplace.id) {
+      throw new Error("Item has to have an id to be updated");
+    }
+    const res = await fetch(`${BASE_URL}/${itemToReplace.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(itemToReplace)
+    });
+    if (!res.ok) {
+      throw new Error("Something went wrong");
+    }
+    const replacedItem = await res.json();
+    return replacedItem;
+  },
+  removeFetch: async function(itemRemove) {
+    console.log("itemRemove from removeFetch", itemRemove);
+    if (!itemRemove.id) {
+      throw new Error("Item has to have an id to be updated");
+    }
+    const res = await fetch(`${BASE_URL}/${itemRemove.id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    if (!res.ok) {
+      throw new Error("Something went wrong");
+    }
+    const removedItem = await res.json();
+    return removedItem;
+  }
+};
 
 export default FetchApi;
