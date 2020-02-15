2e7ca8bad88b0c49a879257b6542a0f669cf86ed
Radosław Grzymała <radoslaw.grzymala@sensilabs.pl>
2020-02-15 23:45:12 +0100

    logger

--- src/service/store/store.js
+++ src/service/store/store.js
@@ -2,8 +2,9 @@ import { createStore, applyMiddleware } from "redux";
 import appReducer from "./reducers";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
+import logger from "redux-logger";
 
 export const store = createStore(
   appReducer,
-  composeWithDevTools(applyMiddleware(thunk))
+  composeWithDevTools(applyMiddleware(logger, thunk))
 );
