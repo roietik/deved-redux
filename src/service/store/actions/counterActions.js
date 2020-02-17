404d4457e14d88d8e34f5977d0ab5522b7675fca
Radosław Grzymała <radoslaw.grzymala@sensilabs.pl>
2020-02-17 12:57:17 +0100

    refactor thunk async actions

--- src/service/store/actions/counterActions.js
+++ src/service/store/actions/counterActions.js
@@ -60,117 +60,103 @@ export const del = idx => {
   };
 };
 
-export const fetchInc = (val, obj, idx) => {
-  return dispatch => {
-    FetchApi.replaceFetch({ num: obj.num + val, id: obj.id })
-      .then(() => {
-        dispatch({
-          type: FETCH_INC,
-          payload: idx,
-          val: val
-        });
-      })
-      .catch(isError => {
-        console.log(isError);
+export const fetchInc = (val, obj, idx) => dispatch => {
+  FetchApi.replaceFetch({ num: obj.num + val, id: obj.id })
+    .then(() => {
+      dispatch({
+        type: FETCH_INC,
+        payload: idx,
+        val: val
       });
-  };
+    })
+    .catch(isError => {
+      console.log(isError);
+    });
 };
 
-export const fetchDec = (val, obj, idx) => {
-  return dispatch => {
-    FetchApi.replaceFetch({ num: obj.num - val, id: obj.id })
-      .then(() => {
-        dispatch({
-          type: FETCH_DEC,
-          payload: idx,
-          val: val
-        });
-      })
-      .catch(isError => {
-        console.log(isError);
+export const fetchDec = (val, obj, idx) => dispatch => {
+  FetchApi.replaceFetch({ num: obj.num - val, id: obj.id })
+    .then(() => {
+      dispatch({
+        type: FETCH_DEC,
+        payload: idx,
+        val: val
       });
-  };
+    })
+    .catch(isError => {
+      console.log(isError);
+    });
 };
 
-export const fetchAdd = (valFromEvent, idx) => {
-  return dispatch => {
-    FetchApi.addFetch({ num: Number(valFromEvent) })
-      .then(() => {
-        dispatch({
-          type: FETCH_ADD,
-          payload: idx,
-          valFromEvent
-        });
-      })
-      .catch(isError => {
-        console.log(isError);
+export const fetchAdd = (valFromEvent, idx) => dispatch => {
+  FetchApi.addFetch({ num: Number(valFromEvent) })
+    .then(() => {
+      dispatch({
+        type: FETCH_ADD,
+        payload: idx,
+        valFromEvent
       });
-  };
+    })
+    .catch(isError => {
+      console.log(isError);
+    });
 };
 
-export const fetchReset = (val, obj, idx) => {
-  return dispatch => {
-    FetchApi.replaceFetch({ num: val, id: obj.id })
-      .then(() => {
-        dispatch({
-          type: FETCH_RESET,
-          payload: idx,
-          val: val
-        });
-      })
-      .catch(isError => {
-        console.log(isError);
+export const fetchReset = (val, obj, idx) => dispatch => {
+  FetchApi.replaceFetch({ num: val, id: obj.id })
+    .then(() => {
+      dispatch({
+        type: FETCH_RESET,
+        payload: idx,
+        val: val
       });
-  };
+    })
+    .catch(isError => {
+      console.log(isError);
+    });
 };
 
-export const fetchChange = (valFromEvent, count, idx) => {
-  return dispatch => {
-    FetchApi.replaceFetch({ num: Number(valFromEvent), id: count.id })
-      .then(() => {
-        dispatch({
-          type: FETCH_CHANGE,
-          payload: idx,
-          valFromEvent
-        });
-      })
-      .catch(isError => {
-        console.log(isError);
+export const fetchChange = (valFromEvent, count, idx) => dispatch => {
+  FetchApi.replaceFetch({ num: Number(valFromEvent), id: count.id })
+    .then(() => {
+      dispatch({
+        type: FETCH_CHANGE,
+        payload: idx,
+        valFromEvent
       });
-  };
+    })
+    .catch(isError => {
+      console.log(isError);
+    });
 };
 
-export const fetchDel = (count, idx) => {
-  return dispatch => {
-    FetchApi.removeFetch(count)
-      .then(() => {
-        dispatch({
-          type: FETCH_DEL,
-          payload: idx
-        });
-      })
-      .catch(isError => {
-        console.log(isError);
+export const fetchDel = (count, idx) => dispatch => {
+  FetchApi.removeFetch(count)
+    .then(() => {
+      dispatch({
+        type: FETCH_DEL,
+        payload: idx
       });
-  };
+    })
+    .catch(isError => {
+      console.log(isError);
+    });
 };
 
 // to jest inaczej bo funkcja asynchroniczna
-export const fetchAll = () => {
-  return dispatch => {
-    FetchApi.getFetch()
-      .then(items => {
-        dispatch({
-          type: FETCH_ALL,
-          payload: items
-        });
-      })
-      .catch(isError => {
-        console.log(isError);
-        dispatch({
-          type: FETCH_ALL,
-          payload: []
-        });
+export const fetchAll = () => dispatch => {
+  FetchApi.getFetch()
+    .then(items => {
+      dispatch({
+        type: FETCH_ALL,
+        payload: items
       });
-  };
+    })
+    .catch(isError => {
+      console.log(isError);
+      dispatch({
+        type: FETCH_ALL,
+        payload: []
+      });
+    });
 };
