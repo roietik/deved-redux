9b7b4526e43d3ffeeaf99d807959644d804cc334
Radosław Grzymała <radoslaw.grzymala@sensilabs.pl>
2020-02-17 13:15:57 +0100

    axios api connect

---
+++ src/service/api/AxiosApi.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+window.axios = axios;
+
+const BASE_URL = "http://localhost:4033/counts";
+
+const AxiosApi = {
+  getAxios: async function() {
+    const res = await axios.get(BASE_URL);
+    const items = res.data;
+    return items;
+  },
+  addAxios: async function(itemToAdd) {
+    const res = await axios.post(BASE_URL, itemToAdd);
+    const addedItem = res.data;
+    return addedItem;
+  },
+  replaceAxios: async function(itemToReplace) {
+    if (!itemToReplace.id) {
+      throw new Error("TimeBos has to have an id to be updated");
+    }
+    const res = await axios.put(
+      `${BASE_URL}/${itemToReplace.id}`,
+      itemToReplace
+    );
+    const replacedItem = res.data;
+    return replacedItem;
+  },
+  removeAxios: async function(itemRemove) {
+    if (!itemRemove.id) {
+      throw new Error("TimeBos has to have an id to be updated");
+    }
+    await axios.delete(`${BASE_URL}/${itemRemove.id}`, itemRemove);
+  }
+};
+
+export default AxiosApi;
